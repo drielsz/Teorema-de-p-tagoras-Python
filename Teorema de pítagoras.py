f302c483b1564fafcad2b45489a635ddb0f21a4a
drielsz <adriellaurentino4@gmail.com>
2022-06-22 16:47:10 -0300

    first commit

---
+++ Teorema de pítagoras.py
@@ -0,0 +1,11 @@
+print("Seja bem vindo. Resolvendo Teorema de Pitágoras em Python. ")
+print("Descubra a hipotenusa")
+b = float(input("Valor do primeiro oposto: "))
+c = float(input("Valor do segundo adjacente: "))
+formula = (b**2 + c**2 ) ** 0.5
+a = 1
+xy  = a**2
+xy = formula
+
+
+print ("Valor da hipotenusa A é: ", formula)
